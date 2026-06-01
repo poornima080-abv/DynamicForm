1c8cc7f9e96be8f2a9f79c5c3ad60665662260b9
poornima0080-abv <praghuvanshi@srtekbox.com>
2026-06-01 11:22:56 +0530

    Your commit message

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -7,64 +7,56 @@ describe('Dashboard Functionality', () => {
     cy.contains('mat-label', 'Password').parents('mat-form-field').find('input').type('P@ssw0rd');
     cy.contains('button', 'Log In').click();
     cy.url().should('include', 'https://srfclm-test.smartcontract.co.in/#/Actionables');
+    cy.contains('Go to Workbench', { timeout: 20000 }).should('be.visible').click();
+    cy.contains('New Contract Request', { timeout: 20000 }).should('be.visible').click();
+
   });
 
   after(() => {
     cy.log('All tests completed');
   });
   Object.entries(categories).forEach(([contractType, fields]) => {
+  it(`Validate ${contractType}`, () => {
+    cy.get('mat-select[title="Contract Type"]')
+  .filter(':visible')
+  .first()
+  .click();
 
-    it(`Validate ${contractType}`, () => {
-      cy.get('body').then(($body) => {
-      cy.contains('New Contract Request', { timeout: 20000 })
-        .should('be.visible')
-        .click();
+   cy.get('mat-option')
+  .should('have.length.greaterThan', 0);
 
-      cy.get('mat-select', { timeout: 20000 })
-        .should('exist')
-        .first()
-        .click();
-        cy.contains('mat-option', contractType)
-          .should('be.visible')
-          .click();
+cy.contains('mat-option', contractType, { timeout: 10000 })
+  .click(); // Step 4: Validate fields
+    cy.wrap(fields).each((field) => {
+      cy.log(`Checking field: ${field.name}`);
+      cy.contains(field.name, { timeout: 15000, matchCase: false }).should('be.visible');
 
- // Step 4: Validate fields
-      cy.wrap(fields).each((field) => {
+      // Validate field type (DOM-based, not formControl-based)
+      cy.contains(field.name, { matchCase: false })
+        .parents('mat-form-field, div')
+        .then(($el) => {
 
-        cy.log(`Checking field: ${field.name}`);
+          const type = (field.type || '').toLowerCase();
 
-       cy.contains(field.name, { timeout: 15000, matchCase: false })
-          .should('be.visible');
+          if (type === 'text') {
+            cy.wrap($el).find('input')
+              .should('exist');
+          }
 
-        // Validate field type (DOM-based, not formControl-based)
-        cy.contains(field.name, { matchCase: false })
-          .parents('mat-form-field, div')
-          .then(($el) => {
+          if (type === 'selection') {
+            cy.wrap($el).find('mat-select')
+              .should('exist');
+          }
 
-            const type = (field.type || '').toLowerCase();
+          if (type === 'textarea') {
+            cy.wrap($el).find('textarea')
+              .should('exist');
+          }
 
-            if (type === 'text') {
-              cy.wrap($el).find('input')
-                .should('exist');
-            }
-
-            if (type === 'selection') {
-              cy.wrap($el).find('mat-select')
-                .should('exist');
-            }
-
-            if (type === 'textarea') {
-              cy.wrap($el).find('textarea')
-                .should('exist');
-            }
-
-          });
-
-      });
+        });
 
     });
-
+  });
   });
 
-});
 });
